9d52a2ebb1aca8746d9dbd6988f88837c5290423
TJ Usiyan <griotspeak@gmail.com>
2015-07-25 22:35:37 +0530

    Initial Commit

---
+++ Rational/Rational.h
@@ -0,0 +1,19 @@
+//
+//  Rational.h
+//  Rational
+//
+//  Created by TJ Usiyan on 7/25/15.
+//  Copyright © 2015 Buttons and Lights LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Rational.
+FOUNDATION_EXPORT double RationalVersionNumber;
+
+//! Project version string for Rational.
+FOUNDATION_EXPORT const unsigned char RationalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rational/PublicHeader.h>
+
+
