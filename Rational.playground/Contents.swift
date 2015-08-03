bd7e0ea244a78498a25d9ce6ea63e1ade0a0e9ca
TJ Usiyan <griotspeak@gmail.com>
2015-08-03 15:50:37 +0530

    prepare for iOS target

--- Rational.playground/Contents.swift
+++ Rational.playground/Contents.swift
@@ -6,10 +6,3 @@ import Rational
 let oneHalf = Rational(0.5)
 let oneFourth = Rational(1, 4)!
 oneHalf + oneFourth + oneFourth
-
-findExponentForNumber(5)
-findExponentForNumber(0.5)
-findExponentForNumber(0.05)
-findExponentForNumber(0.003)
-findExponentForNumber(0.0005)
-
